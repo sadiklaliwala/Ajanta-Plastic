a1f677534524b712d3834920b2851d276adf3f24
sadik <153581673+sadiklaliwala@users.noreply.github.com>
2025-08-13 12:30:24 +0530

    Deployment Code is Added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn apd.wsgi
--- apd/settings.py
+++ apd/settings.py
@@ -9,24 +9,31 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
+from decouple import config, RepositoryEnv
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Point to the .env file (in the root directory)
+env_path = BASE_DIR / '.env'
+# config = Config(RepositoryEnv(env_path))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=bx$g=8r%$-(16a8s7yb5zkqhz=!*jrarw6ey7j79&of*smxfz'
+# SECRET_KEY = 'django-insecure-=bx$g=8r%$-(16a8s7yb5zkqhz=!*jrarw6ey7j79&of*smxfz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,19 +84,9 @@ WSGI_APPLICATION = 'apd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'apd',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'PORT': 4306,
-        'PASSWORD':''
-
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
