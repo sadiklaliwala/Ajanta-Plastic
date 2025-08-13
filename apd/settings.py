bcc96d455c14789bcad9cf404251f243990a134e
sadik <153581673+sadiklaliwala@users.noreply.github.com>
2025-08-13 20:03:11 +0530

    New DataBase Add_on Is added

--- apd/settings.py
+++ apd/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'apd.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME','apd'),
-        'USER': os.environ.get('DB_USER','root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD',''),
-        'HOST': os.environ.get('DB_HOST','localhost'),
-        'PORT': os.environ.get('DB_PORT',4306),
+        'NAME': os.environ.get('MYSQL_ADDON_DB','apd'),
+        'USER': os.environ.get('MYSQL_ADDON_USER','root'),
+        'PASSWORD': os.environ.get('MYSQL_ADDON_PASSWORD',''),
+        'HOST': os.environ.get('MYSQL_ADDON_HOST','localhost'),
+        'PORT': os.environ.get('MYSQL_ADDON_PORT',4306)
     }
 }
 # DATABASES = {
